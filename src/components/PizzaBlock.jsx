74582789bac61c2efcef3ca01a6569d61493ebfe
web_artur_work <web.artur.work@gmail.com>
2024-08-11 01:47:46 +0200

    Create categories functions

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,5 +1,5 @@
-export default function PizzaBlock(props) {
-    console.log(props.title)
+export default function PizzaBlock({title, price}) {
+
     return (
         <div className="pizza-block">
             <img
@@ -7,7 +7,7 @@ export default function PizzaBlock(props) {
                 src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
                 alt="Pizza"
             />
-            <h4 className="pizza-block__title">{props.title}</h4>
+            <h4 className="pizza-block__title">{title}</h4>
             <div className="pizza-block__selector">
                 <ul>
                     <li className="active">тонкое</li>
@@ -21,8 +21,8 @@ export default function PizzaBlock(props) {
                 </ul>
             </div>
             <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от {props.price} ₽</div>
-                <div className="button button--outline button--add">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button className="button button--outline button--add">
                     <svg
                         width="12"
                         height="12"
@@ -37,7 +37,7 @@ export default function PizzaBlock(props) {
                     </svg>
                     <span>Добавить</span>
                     <i>2</i>
-                </div>
+                </button>
             </div>
         </div>
     )
