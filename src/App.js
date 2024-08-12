d1c31b41113e6cba1b22347c2075a615ab0ee15e
web_artur_work <web.artur.work@gmail.com>
2024-08-12 03:36:39 +0200

    Create Skeleton component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import PizzaSkeleton from "./components/PizzaBlock/Skeleton";
 
 import './scss/app.scss';
 
@@ -15,6 +16,7 @@ const apiKey = '12345';
 function App() {
 
   const [items, setItems] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
     fetch(apiUrl, {
@@ -27,6 +29,7 @@ function App() {
       return res.json()
     }).then((arr) => {
       setItems(arr);
+      setIsLoading(false)
     })
   }, []);
 
@@ -45,9 +48,10 @@ function App() {
               <h2 className="content__title">Все пиццы</h2>
               <div className="content__items">
                 {
-                  items.map((obj) =>
-                  <PizzaBlock key={obj.id}{...obj} />
-                )}
+                  isLoading
+                      ? [...new Array(6)].map((_, index) => (<PizzaSkeleton key={index} />))
+                      : items.map((obj) => <PizzaBlock key={obj.id}{...obj} />)
+                }
               </div>
             </div>
           </div>
